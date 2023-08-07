441f7c381e6df55e347f6c0a422746868d957715
Bradkibs <Bradleykibs@gmail.com>
2023-08-07 22:03:36 +0300

    unauthorized

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,9 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized/', strict_slashes=False)
+def unauth_endpoint():
+    """called when someone accesses unauthorized enpoint"""
+    abort(401)
