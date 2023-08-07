9dfd28a6a347eeb9bd82b875bb177b5eadd5f2f9
Bradkibs <Bradleykibs@gmail.com>
2023-08-07 22:06:18 +0300

    unauthorized fix

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -22,9 +22,11 @@ def not_found(error) -> str:
 
 
 @app.errorhandler(401)
-def unauthorized(error) ->str:
+def unauthorized(error) -> str:
     """An unauthorized handler"""
     return jsonify({"error": "Unauthorized"}), 401
+
+
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
