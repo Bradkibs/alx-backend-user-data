f2daa6d58f3aeafdbe54013844b1afb9b8963115
Bradkibs <Bradleykibs@gmail.com>
2023-08-09 18:39:21 +0300

    finishing the getting user id function

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -14,8 +14,14 @@ class SessionAuth(Auth):
 
     def create_session(self, user_id: str = None) -> str:
         """creates a session ID for a given user_id"""
-        if user_id == None or not isinstance(user_id, str):
+        if user_id is None or not isinstance(user_id, str):
             return None
         sess_id = uuid.uuid4()
         self.user_id_by_session_id[str(sess_id)] = user_id
         return str(sess_id)
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """returns a User Id based on a session_id"""
+        if session_id is None or not isinstance(session_id, str):
+            return None
+        return self.user_id_by_session_id.get(session_id)
