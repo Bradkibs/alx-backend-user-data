bc21a0d3db0be07787ee7839bc9bbeb203fe9547
Bradkibs <Bradleykibs@gmail.com>
2023-08-10 14:50:05 +0300

    current user

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -25,3 +25,9 @@ class SessionAuth(Auth):
         if session_id is None or not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """returns a User insance based on the cookie value"""
+        U_id = self.user_id_for_session_id(self.session_cookie(request))
+        usr = User.get(U_id)
+        return usr
