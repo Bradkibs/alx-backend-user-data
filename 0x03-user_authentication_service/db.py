534842862d1eccf931702a7c9168e706804e35a7
Bradkibs <Bradleykibs@gmail.com>
2023-08-16 19:02:52 +0300

    implementing find_user_by method with import fix

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -3,6 +3,8 @@
 """
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 from user import Base, User
@@ -48,6 +50,8 @@ class DB:
             user = self._session.query(User).filter_by(**kwargs).first()
             if not user:
                 raise NoResultFound("No results found")
+        except NoResultFound:
+            raise NoResultFound("No results found")
         except InvalidRequestError:
             raise InvalidRequestError("not a valid request")
         return user
