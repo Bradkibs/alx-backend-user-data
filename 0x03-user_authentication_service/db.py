42c09ca5c1dec3ec4aa4dc5c3aa2fe23dea1f6c7
Bradkibs <Bradleykibs@gmail.com>
2023-08-16 19:06:37 +0300

    implementing find_user_by method with import fix

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -31,7 +31,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str) -> User:
+    def add_user(self, email: str, hashed_password: str):
         """saves the user to the db if all requirements are passed"""
         if isinstance(email, str) and isinstance(hashed_password, str):
             try:
