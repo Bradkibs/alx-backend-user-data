af740c2337891df20ff004ab7de6a80731460d7d
Bradkibs <Bradleykibs@gmail.com>
2023-08-16 19:17:18 +0300

    fix

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """DB module
 """
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, tuple_
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.exc import InvalidRequestError
@@ -38,20 +38,24 @@ class DB:
                 user = User(email=email, hashed_password=hashed_password)
                 self._session.add(user)
                 self._session.commit()
-                return user
-
             except Exception as exp:
                 self._session.rollback()
-                raise exp
+                user = None
+            return user
 
     def find_user_by(self, **kwargs) -> User:
-        """returns the first user it matches or throws errors"""
-        try:
-            user = self._session.query(User).filter_by(**kwargs).first()
-            if not user:
-                raise NoResultFound()
-        except NoResultFound:
+        """Finds a user based on a set of filters.
+        """
+        fields, values = [], []
+        for key, value in kwargs.items():
+            if hasattr(User, key):
+                fields.append(getattr(User, key))
+                values.append(value)
+            else:
+                raise InvalidRequestError()
+        result = self._session.query(User).filter(
+            tuple_(*fields).in_([tuple(values)])
+        ).first()
+        if result is None:
             raise NoResultFound()
-        except InvalidRequestError:
-            raise InvalidRequestError()
-        return user
+        return result
