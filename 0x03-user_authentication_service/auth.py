190ab72004a393e1844a6f6be7ad37fd55e5a3c9
Bradkibs <Bradleykibs@gmail.com>
2023-08-17 14:35:06 +0300

    implementing authentication

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,10 +2,31 @@
 """Hashing the password method"""
 
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.exc import NoResultFound
 
 
-def _hash_password(password: str) -> str:
-    """takes in the password and returns bytes of a salted hash"""
-    salt = bcrypt.gensalt()
-    hashed_pass = bcrypt.hashpw(password.encode('utf-8'), salt)
-    return hashed_pass.decode('utf-8')
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Registering a user and making sure that the user is valid"""
+        try:
+            user = self._db.find_user_by(email=email)
+            if user:
+                raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            passw = self._hash_password(password)
+            new_user = self._db.add_user(email=email, hashed_password=passw)
+            return new_user
+
+    def _hash_password(self, password: str) -> str:
+        """takes in the password and returns bytes of a salted hash"""
+        salt = bcrypt.gensalt()
+        hashed_pass = bcrypt.hashpw(password.encode('utf-8'), salt)
+        return hashed_pass.decode('utf-8')
