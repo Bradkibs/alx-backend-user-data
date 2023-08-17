5cb3891966d417ef2f18f66ebe6beea0090bf171
Bradkibs <Bradleykibs@gmail.com>
2023-08-17 18:08:08 +0300

    Deleting sessions fix

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Setting up a basic flask app"""
 
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request, abort, redirect
 from auth import Auth
 
 
